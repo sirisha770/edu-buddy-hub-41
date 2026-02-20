86ec30cb312514fd46dce83b1dd766e07b405ffe
sirisha <sirisha4541kuppili@gmail.com>
2026-02-20 14:49:10 +0530

    changes have done

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/edu-buddy-hub-41">
           <AuthProvider>
             <Routes>
               {/* Public */}
